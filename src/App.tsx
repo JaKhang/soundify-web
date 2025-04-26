2db4ed6fb5d44e5fc53308f99231671c7c776938
JaKhang <kamilionbc@gmail.com>
2025-04-26 19:03:35 +0700

    Remove unused styles and redundant code
    Eliminated unused CSS files and inline styles to simplify the codebase. This cleanup reduces redundancy, improves maintainability, and prepares the project for future styling updates.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import './theme/theme.css'
-import './App.module.css'
-import {useState} from "react";
-import "./App.module.css"
 
 function App() {
-    const [isPlaying, setPlaying] = useState(false)
     return (
-        <div style={{width: "100vw", backgroundColor: "var(--background-dark-paper)"}}>
+        <div>
             App
         </div>
     )
